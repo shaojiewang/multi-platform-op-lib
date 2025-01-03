9ce4b020329b68062e5952674bffc774d30cc6e8
Perloc423 <perloc423@gmail.com>
2025-01-03 09:02:41 +0000

    correct build sh

--- csrc/memcpy_example_gfx942/build_clanghipcc.sh
+++ csrc/memcpy_example_gfx942/build_clanghipcc.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-SRC=test.cpp
+SRC=main.cpp
 TARGET=out
 
 for KSRC in *.s; do
-    if [ -f "$KSRC" ]; then
-        KOUT="${KSRC%.s}.hsaco"
-        rm -rf $KOUT
+  if [ -f "$KSRC" ]; then
+    KOUT="${KSRC%.s}.hsaco"
+    rm -rf $KOUT
 
-        /opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa --offload-arch=gfx942:sramecc+:xnack- "$KSRC" -o "$KOUT"
-    fi
+    /opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa --offload-arch=gfx942:sramecc+:xnack- "$KSRC" -o "$KOUT"
+  fi
 done
 rm -rf $TARGET
 /opt/rocm/bin/hipcc $SRC --offload-arch=gfx942:sramecc+:xnack- -o $TARGET
-
