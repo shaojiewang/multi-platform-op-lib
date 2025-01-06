fa7966a746033901224b63bc79cd75a0ca6d797b
shaojiewang <wsjmessi@163.com>
2025-01-06 04:11:01 +0000

    remove uploaded executable

--- csrc/mma_operation/build.sh
+++ csrc/mma_operation/build.sh
@@ -1,8 +1,8 @@
 ARCH=gfx942
 /opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=$ARCH mma_inst.s -o kernel.co
-/opt/rocm/bin/hipcc --offload-arch=$ARCH  test.cu -o test
+/opt/rocm/bin/hipcc --offload-arch=$ARCH  test.cu -o test.exe
 
 # run
-./test v_mfma_f32_16x16x16_bf16 16 16 16 1 16 2100 
+./test.exe v_mfma_f32_16x16x16_bf16 16 16 16 1 16 2100 
 #./test v_mfma_f32_32x32x8_bf16 32 32 8 1 32 2100 
 
