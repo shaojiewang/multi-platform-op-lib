ba12ca5ef651cc16c54aef7441040b2cf66a4c3d
shaojiewang <wsjmessi@163.com>
2025-01-10 09:33:58 +0000

    add rocm code

---
+++ csrc/mma_operation/mma_rocm.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "bf16_wrapper.hpp"
+
+#include <hip/hip_runtime.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using int32x4_t = int32_t __attribute__((ext_vector_type(4)));
+#define BUFFER_LOAD_DWORD3 0x00020000   // This is valid for 
+struct buffer_resource {
+    const void * ptr;
+    uint32_t range;
+    uint32_t config;
+};
+__device__ int32x4_t make_buffer_resource(const void * ptr, uint32_t size = 0xffffffff)
+{
+    buffer_resource res {ptr, size, BUFFER_LOAD_DWORD3};
+    return __builtin_bit_cast(int32x4_t, res);
+}
+
+
+
