0b11de07122dd097f62694d9f289780d6ce9e891
shaojiewang <wsjmessi@163.com>
2025-01-21 22:06:17 +0800

    add hopper mma header file code

--- csrc/mma_operation/mma_cuda.hpp
+++ csrc/mma_operation/mma_cuda.hpp
@@ -36,10 +36,12 @@ using half_t = half;
 #define HOST_DEVICE __forceinline__ __host__ __device__
 #define DEVICE __forceinline__ __device__
 
-int get_sm_count() {
+int get_sm_count() 
+{
   int device_id;
   cudaError_t result = cudaGetDevice(&device_id);
-  if (result != cudaSuccess) {
+  if (result != cudaSuccess) 
+  {
     std::cerr << "cudaGetDevice() returned error " << cudaGetErrorString(result)
               << std::endl;
     return 1;
@@ -47,13 +49,32 @@ int get_sm_count() {
   int multiprocessor_count;
   result = cudaDeviceGetAttribute(&multiprocessor_count,
                                   cudaDevAttrMultiProcessorCount, device_id);
-  if (result != cudaSuccess) {
+  if (result != cudaSuccess) 
+  {
     std::cerr << "cudaDeviceGetAttribute() returned error "
               << cudaGetErrorString(result) << std::endl;
     return 1;
   }
   return multiprocessor_count;
 }
+
+DEVICE void warpgroup_arrive() 
+{
+  asm volatile("wgmma.fence.sync.aligned;\n" ::: "memory");
+}
+
+DEVICE void warpgroup_commit_batch()
+{
+  asm volatile("wgmma.commit_group.sync.aligned;\n" ::: "memory");
+}
+
+template <int N>
+DEVICE void warpgroup_wait() 
+{
+  static_assert(N >= 0 && N <=7, "wgmma wait: N must be in range[0, 7]");
+  asm volatile("wgmma.wait_group.sync.aligned %0;\n" ::"n"(N) : "memory");
+}
+
 
 static constexpr int MAX_CLUSTER_SIZE = 16;
 static constexpr int WARP_GROUP_SIZE = 128;
@@ -108,4 +129,81 @@ struct SM90_64x128x16_F32BF16BF16_SS {
   }
 };
 
+union MMASmemDescriptor
+{
+  uint64_t desc_;
+  uint32_t reg32_[2];
+  uint16_t reg16_[4];
 
+  struct
+  {
+    uint16_t start_address_ : 14, : 2;
+    uint16_t leading_byte_offset_ : 14, : 2;
+    uint16_t stride_byte_offset_ : 14, : 2;
+    uint8_t : 1, base_offset_ : 3, : 4;
+    uint8_t : 6, layout_type_ : 2;
+  } bitfield;
+};
+
+/// make shared memory descriptor
+template <class PointerType>
+DEVICE MMASmemDescriptor make_smem_desc(PointerType smem_ptr) 
+{
+  MMASmemDescriptor desc;
+  uint32_t uint_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(smem_ptr));
+  desc.bitfield.start_address_ = uint_ptr >> 4;
+  desc.bitfield.layout_type_ =
+      0x1;  /// swizzle 128B because we use Swizzle<3,4,3>
+  desc.bitfield.leading_byte_offset_ = 0x1;  /// no use
+  desc.bitfield.stride_byte_offset_ =
+      64;  /// how many 128bits-rows needed between two core matrices
+  desc.bitfield.base_offset_ = 0x0;
+  return desc;
+}
+
+template <class AType, 
+          class BType>
+struct KernelSharedStorage {
+  alignas(128) AType smem_a[40 * 1024];
+  alignas(128) BType smem_b[40 * 1024];
+};
+
+template <class TA,
+          class TB,
+          class TAcc>
+__global__ void wgmma_block(TAcc* acc_ptr)
+{
+  int tidx = threadIdx.x;
+  int bidx = blockIdx.x;
+  int offset = bidx * blockDim.x + tidx;
+
+  TAcc accumulators[64];
+
+  extern __shared__ uint8_t raw_shared_mem[];
+
+  KernelStorage<TA, TB>& shared_storage = *reinterpret_cast<KernelStorage<TA, TB>*>(raw_shared_mem);
+
+  warpgroup_arrive();
+  
+  auto desc_a = make_smem_desc(shared_storage.smem_a);
+  auto desc_b = make_smem_desc(shared_storage.smem_b);
+
+  SM90_64x128x16_F32BF16BF16_SS<1, 1, 0, 0, 0>::wgmma(desc_a, desc_b, 
+    accumulators[0], accumulators[1], accumulators[2], accumulators[3],
+    accumulators[4], accumulators[5], accumulators[6], accumulators[7],
+    accumulators[8], accumulators[9], accumulators[10], accumulators[11],
+    accumulators[12], accumulators[13], accumulators[14], accumulators[15],
+    accumulators[16], accumulators[17], accumulators[18], accumulators[19],
+    accumulators[20], accumulators[21], accumulators[22], accumulators[23],
+    accumulators[24], accumulators[25], accumulators[26], accumulators[27],
+    accumulators[28], accumulators[29], accumulators[30], accumulators[31],
+    accumulators[32], accumulators[33], accumulators[34], accumulators[35],
+    accumulators[36], accumulators[37], accumulators[38], accumulators[39],
+    accumulators[40], accumulators[41], accumulators[42], accumulators[43],
+    accumulators[44], accumulators[45], accumulators[46], accumulators[47],
+    accumulators[48], accumulators[49], accumulators[50], accumulators[51],
+    accumulators[52], accumulators[53], accumulators[54], accumulators[55],
+    accumulators[56], accumulators[57], accumulators[58], accumulators[59],
+    accumulators[60], accumulators[61], accumulators[62], accumulators[63]
+  );
+}
