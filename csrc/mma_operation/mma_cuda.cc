c5d51c00b51c1e6abe8779d3b22bb19cb60ff716
shaojiewang <wsjmessi@163.com>
2025-01-24 14:53:06 +0800

    add compile shell

--- csrc/mma_operation/mma_cuda.cc
+++ csrc/mma_operation/mma_cuda.cc
@@ -4,9 +4,10 @@
 
 #include "mma_cuda.hpp"
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
   int num_cu = get_sm_count();
+  cudaEvent_t event_start, event_end;
   int total_loop= 10;
   int warm_ups = 5;
   int i;
@@ -23,29 +24,29 @@ int main(int argc, char* argv)
   float random_seed = ((float)(rand() % 1000))/1000.0;
   
   void* ptr_in;
-  HIP_CALL(hipMalloc(&ptr_in, gdx * bdx * sizeof(bf16x4_t) * inst_iter * (1 + total_loop))); 
+  CUDA_CHECK(cudaMalloc(&ptr_in, gdx * bdx * sizeof(bf16x4_t) * inst_iter * (1 + total_loop))); 
      
   for(i = 0; i < warm_ups; i++)
   {
     mma_launcher(ptr_in, random_seed, inst_iter, gdx, bdx);
   }
-  HIP_CALL(hipEventCreate(&event_start));
-  HIP_CALL(hipEventCreate(&event_end));
+  CUDA_CHECK(cudaEventCreate(&event_start));
+  CUDA_CHECK(cudaEventCreate(&event_end));
 
-  (hipCtxSynchronize());
-  HIP_CALL(hipEventRecord(event_start, NULL));
+  (cuCtxSynchronize());
+  CUDA_CHECK(cudaEventRecord(event_start, NULL));
   for(i = 0; i < total_loop; i++)
   {
     char* tmp_ptr_in = reinterpret_cast<char*>(ptr_in) + gdx * bdx * sizeof(bf16x8_t) * (i + 1);
     mma_launcher(reinterpret_cast<void*>(tmp_ptr_in), random_seed, inst_iter, gdx, bdx);
   }
   float elapsed_ms;
-  HIP_CALL(hipEventRecord(event_end, NULL));
-  HIP_CALL(hipEventSynchronize(event_end));
-  (hipCtxSynchronize());
-  HIP_CALL(hipEventElapsedTime(&elapsed_ms, event_start, event_end));
-  HIP_CALL(hipEventDestroy(event_start));
-  HIP_CALL(hipEventDestroy(event_end));
+  CUDA_CHECK(cudaEventRecord(event_end, NULL));
+  CUDA_CHECK(cudaEventSynchronize(event_end));
+  (cuCtxSynchronize());
+  CUDA_CHECK(cudaEventElapsedTime(&elapsed_ms, event_start, event_end));
+  CUDA_CHECK(cudaEventDestroy(event_start));
+  CUDA_CHECK(cudaEventDestroy(event_end));
 
   float time_per_loop = elapsed_ms / total_loop;
   //float tips = (double)inst_loop*inst_blocks*num_cu*bdx/time_per_loop/1e9;
@@ -55,7 +56,7 @@ int main(int argc, char* argv)
 
   double Tflops = (double)2 * M * N * K * blocks * 4 * num_cu * (32 * inst_iter) / time_per_loop / 1e9;
   double Gflop = (double)2 * M * N * K * blocks * 4 * num_cu * (32 * inst_iter)  / 1e9;
-  double TheTflops = (double)2 * M * N * K * blocks * 4 * num_cu * SCLK / cycles / 1e3;
+  double TheTflops = 989.0; // (double)2 * M * N * K * blocks * 4 * num_cu * SCLK / cycles / 1e3;
   float RelPerf = Tflops / TheTflops;
 
   printf("%d, %-32s, %i, %.1f, %.3fms, %.2f, %.1f, %.3f \n", num_cu, argv[1], MHZ, Tflops, time_per_loop, Gflop, TheTflops, RelPerf);
