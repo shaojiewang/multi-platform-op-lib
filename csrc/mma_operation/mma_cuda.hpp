a5eb702418b1d7c8cf1701cfd4cb8c5eadb22bb8
shaojiewang <wsjmessi@163.com>
2025-07-17 08:43:04 +0000

    add wgmma perf verification

--- csrc/mma_operation/mma_cuda.hpp
+++ csrc/mma_operation/mma_cuda.hpp
@@ -186,6 +186,8 @@ __global__ void wgmma_block(TAcc* acc_ptr, int inst_iter, float random_seed)
 
   TAcc accumulators[64];
 
+  //printf("in kernel begin inst_iter=%d\n", inst_iter);
+
   for(int i = 0; i < 64; i++)
   {
     accumulators[i] = 0;
@@ -206,7 +208,7 @@ __global__ void wgmma_block(TAcc* acc_ptr, int inst_iter, float random_seed)
 #pragma unroll
     for(int j = 0; j < ITER; j++)
     {
-      SM90_64x128x16_F32BF16BF16_SS<1, 1, 0, 0, 0>::wgmma(desc_a.desc_, desc_b.desc_, 
+      SM90_64x128x16_F32BF16BF16_SS<1, 1, 1, 0, 0>::wgmma(desc_a.desc_, desc_b.desc_, 
         accumulators[0], accumulators[1], accumulators[2], accumulators[3],
         accumulators[4], accumulators[5], accumulators[6], accumulators[7],
         accumulators[8], accumulators[9], accumulators[10], accumulators[11],
@@ -226,78 +228,17 @@ __global__ void wgmma_block(TAcc* acc_ptr, int inst_iter, float random_seed)
       );
       step += 512;
       desc_a = make_smem_desc(shared_storage.smem_a + step);
-#if 1
-      SM90_64x128x16_F32BF16BF16_SS<1, 1, 0, 0, 0>::wgmma(desc_a.desc_, desc_b.desc_, 
-        accumulators[0], accumulators[1], accumulators[2], accumulators[3],
-        accumulators[4], accumulators[5], accumulators[6], accumulators[7],
-        accumulators[8], accumulators[9], accumulators[10], accumulators[11],
-        accumulators[12], accumulators[13], accumulators[14], accumulators[15],
-        accumulators[16], accumulators[17], accumulators[18], accumulators[19],
-        accumulators[20], accumulators[21], accumulators[22], accumulators[23],
-        accumulators[24], accumulators[25], accumulators[26], accumulators[27],
-        accumulators[28], accumulators[29], accumulators[30], accumulators[31],
-        accumulators[32], accumulators[33], accumulators[34], accumulators[35],
-        accumulators[36], accumulators[37], accumulators[38], accumulators[39],
-        accumulators[40], accumulators[41], accumulators[42], accumulators[43],
-        accumulators[44], accumulators[45], accumulators[46], accumulators[47],
-        accumulators[48], accumulators[49], accumulators[50], accumulators[51],
-        accumulators[52], accumulators[53], accumulators[54], accumulators[55],
-        accumulators[56], accumulators[57], accumulators[58], accumulators[59],
-        accumulators[60], accumulators[61], accumulators[62], accumulators[63]
-      );
-      step += 512;
-      desc_a = make_smem_desc(shared_storage.smem_a + step);
-#endif
-#if 1
-      SM90_64x128x16_F32BF16BF16_SS<1, 1, 0, 0, 0>::wgmma(desc_a.desc_, desc_b.desc_, 
-        accumulators[0], accumulators[1], accumulators[2], accumulators[3],
-        accumulators[4], accumulators[5], accumulators[6], accumulators[7],
-        accumulators[8], accumulators[9], accumulators[10], accumulators[11],
-        accumulators[12], accumulators[13], accumulators[14], accumulators[15],
-        accumulators[16], accumulators[17], accumulators[18], accumulators[19],
-        accumulators[20], accumulators[21], accumulators[22], accumulators[23],
-        accumulators[24], accumulators[25], accumulators[26], accumulators[27],
-        accumulators[28], accumulators[29], accumulators[30], accumulators[31],
-        accumulators[32], accumulators[33], accumulators[34], accumulators[35],
-        accumulators[36], accumulators[37], accumulators[38], accumulators[39],
-        accumulators[40], accumulators[41], accumulators[42], accumulators[43],
-        accumulators[44], accumulators[45], accumulators[46], accumulators[47],
-        accumulators[48], accumulators[49], accumulators[50], accumulators[51],
-        accumulators[52], accumulators[53], accumulators[54], accumulators[55],
-        accumulators[56], accumulators[57], accumulators[58], accumulators[59],
-        accumulators[60], accumulators[61], accumulators[62], accumulators[63]
-      );
-      step += 512;
-      desc_a = make_smem_desc(shared_storage.smem_a + step);
-#endif
-#if 1
-      SM90_64x128x16_F32BF16BF16_SS<1, 1, 0, 0, 0>::wgmma(desc_a.desc_, desc_b.desc_, 
-        accumulators[0], accumulators[1], accumulators[2], accumulators[3],
-        accumulators[4], accumulators[5], accumulators[6], accumulators[7],
-        accumulators[8], accumulators[9], accumulators[10], accumulators[11],
-        accumulators[12], accumulators[13], accumulators[14], accumulators[15],
-        accumulators[16], accumulators[17], accumulators[18], accumulators[19],
-        accumulators[20], accumulators[21], accumulators[22], accumulators[23],
-        accumulators[24], accumulators[25], accumulators[26], accumulators[27],
-        accumulators[28], accumulators[29], accumulators[30], accumulators[31],
-        accumulators[32], accumulators[33], accumulators[34], accumulators[35],
-        accumulators[36], accumulators[37], accumulators[38], accumulators[39],
-        accumulators[40], accumulators[41], accumulators[42], accumulators[43],
-        accumulators[44], accumulators[45], accumulators[46], accumulators[47],
-        accumulators[48], accumulators[49], accumulators[50], accumulators[51],
-        accumulators[52], accumulators[53], accumulators[54], accumulators[55],
-        accumulators[56], accumulators[57], accumulators[58], accumulators[59],
-        accumulators[60], accumulators[61], accumulators[62], accumulators[63]
-      );
-      step += 512;
-      desc_a = make_smem_desc(shared_storage.smem_a + step);
-#endif
+      desc_b = make_smem_desc(shared_storage.smem_b + step);
     }
     warpgroup_commit_batch();
     warpgroup_wait<0>();
+    __syncwarp();
   }
 
-  // __syncwarp();
+  //if(blockIdx.x == 1 && threadIdx.x == 1)
+  //  printf("in kernel inst_iter=%d\n", inst_iter);
+
+  __syncwarp();
   
   *(acc_ptr + tidx) = accumulators[0];
 }
@@ -315,7 +256,7 @@ void mma_launcher(TAcc* acc_ptr, float random_seed, int inst_iter, uint32_t gdx,
         Kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smemSizeBytes);
     CUDA_CHECK(result);
   }
-  void* kernel_params[] = {&acc_ptr, &inst_iter, &inst_iter};
+  void* kernel_params[] = {&acc_ptr, &inst_iter, &random_seed};
   cudaLaunchConfig_t launch_config;
   launch_config.gridDim = {gdx, 1, 1};
   launch_config.blockDim = {bdx, 1, 1};
